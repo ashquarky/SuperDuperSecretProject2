d08758dad90770b5177191c7280a6396585235f5
Ash <quarktheawesome@gmail.com>
2016-11-05 18:02:27 +1100

    Add WUT example project

---
+++ src/main.c
@@ -0,0 +1,43 @@
+#include <coreinit/core.h>
+#include <coreinit/debug.h>
+#include <coreinit/foreground.h>
+#include <proc_ui/procui.h>
+
+//Main application loop, called each frame.
+//Retrun true to quit app.
+bool MainLoop() {
+	return false;
+}
+
+//Main ProcUI loop, handles many cool things
+void RunApplication() {
+	while (1) {
+		if(!OSIsMainCore()) {
+			ProcUISubProcessMessages(true);
+		}
+		else {
+			ProcUIStatus status = ProcUIProcessMessages(true);
+
+			if(status == PROCUI_STATUS_IN_FOREGROUND) { //Run application
+				if (MainLoop()) break;
+				//TODO: GX2WaitForVsync();
+			} else if(status == PROCUI_STATUS_RELEASE_FOREGROUND) { //Clean up foreground stuff
+				ProcUIDrawDoneRelease();
+			} else if(status == PROCUI_STATUS_EXITING) { //Get ready to exit
+				ProcUIShutdown();
+				break;
+			}
+		}
+	}
+}
+
+//TODO learn about this function, when it's called ETC
+void SaveCallback() {
+	OSSavesDone_ReadyToRelease();
+}
+
+int main(int argc, char **argv) {
+	ProcUIInit(&SaveCallback);
+	RunApplication();
+	return 0;
+}
