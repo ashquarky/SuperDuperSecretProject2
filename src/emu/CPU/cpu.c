074d651ea21cceef49874295da84917fc3c745de
Ash <quarktheawesome@gmail.com>
2016-11-05 18:15:36 +1100

    Add files for CPU

---
+++ src/emu/CPU/cpu.c
@@ -0,0 +1,3 @@
+/*	SuperDuperSecretProject2 - An NES Emulator for the Wii U
+	https://github.com/QuarkTheAwesome/SuperDuperSecretProject2
+	I'd appreciate it if you didn't post this on GBATemp or anything until I think up a name */
--- src/main.c
+++ src/main.c
@@ -1,3 +1,7 @@
+/*	SuperDuperSecretProject2 - An NES Emulator for the Wii U
+	https://github.com/QuarkTheAwesome/SuperDuperSecretProject2
+	I'd appreciate it if you didn't post this on GBATemp or anything until I think up a name */
+
 #include <coreinit/core.h>
 #include <coreinit/debug.h>
 #include <coreinit/foreground.h>
